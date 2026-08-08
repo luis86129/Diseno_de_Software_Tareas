ea910f7f7b580cfb5ac27376c73c69a1068154f4
Luis Vargas <levs3107@gmail.com>
2026-08-08 11:37:46 -0500

    Archivo Tarea 3

---
+++ Tarea03_Pruebas_y_Refactoring_Grupo2/CruiseBook_Project/src/main/java/com/mycompany/cruisebook_project/CruiseBook_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cruisebook_project;
+
+/**
+ *
+ * @author User
+ */
+public class CruiseBook_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
